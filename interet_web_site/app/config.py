435c51e648438a4a3869df1e7260ebdc8d500591
unknown <lmboyenval@free.fr>
2026-05-09 21:48:12 +0200

    add app folder path in config folder

--- interet_web_site/app/config.py
+++ interet_web_site/app/config.py
@@ -3,4 +3,6 @@ import os
 class Config:
     SQLALCHEMY_DATABASE_URI = "sqlite:///devices.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    UPLOAD_FOLDER = "static/uploads"
+    BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+
+    UPLOAD_FOLDER = os.path.join(BASE_DIR, "uploads")
